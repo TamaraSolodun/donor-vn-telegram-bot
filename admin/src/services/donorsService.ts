e5f3bfcbcacda063695265af0885c86976ae1146
Tamara Solodun <toma.solodun@gmail.com>
2024-05-02 10:46:20 +0300

    resolved conflict

--- admin/src/services/donorsService.ts
+++ admin/src/services/donorsService.ts
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import { Donor, DonorList, donorListSchema, donorSchema } from "../interfaces/Donor";
-
-const apiUrl = "http://localhost:8000/api";
-=======
 import {
   Donor,
   DonorList,
@@ -11,7 +6,6 @@ import {
 } from '../interfaces/Donor';
 
 const apiUrl = 'http://localhost:8000/api';
->>>>>>> 02e2af05956d7407bf2e0823deced1fff6ef8853
 
 export const getDonors = async (): Promise<DonorList> => {
   const response = await fetch(`${apiUrl}/donors`);
@@ -23,11 +17,7 @@ export const getDonors = async (): Promise<DonorList> => {
   const parsedData = donorListSchema.parse(data);
 
   return parsedData;
-<<<<<<< HEAD
-}
-=======
 };
->>>>>>> 02e2af05956d7407bf2e0823deced1fff6ef8853
 
 export const getSingleDonor = async (donorId: number): Promise<Donor> => {
   const response = await fetch(`${apiUrl}/donors/${donorId}`);
@@ -39,21 +29,6 @@ export const getSingleDonor = async (donorId: number): Promise<Donor> => {
   const parsedData = donorSchema.parse(data);
 
   return parsedData;
-<<<<<<< HEAD
-}
-
-
-
-export const sendMessages = async (
-  selectedUserIds: number[],
-  bloodGroup: string
-): Promise<void> => {
-  try {
-    const response = await fetch(`${apiUrl}/sendMessages`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-=======
 };
 
 export const sendMessages = async (
@@ -65,21 +40,11 @@ export const sendMessages = async (
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
->>>>>>> 02e2af05956d7407bf2e0823deced1fff6ef8853
       },
       body: JSON.stringify({ selectedUserIds, bloodGroup }),
     });
 
     if (response.ok) {
-<<<<<<< HEAD
-      console.log("Messages sent successfully!");
-    } else {
-      console.error("Failed to send messages:", await response.text());
-      throw new Error(await response.text());
-    }
-  } catch (error) {
-    console.error("Error sending messages:", error);
-=======
       console.log('Messages sent successfully!');
     } else {
       console.error('Failed to send messages:', await response.text());
@@ -87,7 +52,6 @@ export const sendMessages = async (
     }
   } catch (error) {
     console.error('Error sending messages:', error);
->>>>>>> 02e2af05956d7407bf2e0823deced1fff6ef8853
     throw error;
   }
 };
@@ -95,29 +59,14 @@ export const sendMessages = async (
 export const updateDonor = async (updatedDonor: Donor): Promise<void> => {
   try {
     const response = await fetch(`${apiUrl}/donors/${updatedDonor.userId}`, {
-<<<<<<< HEAD
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-=======
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
->>>>>>> 02e2af05956d7407bf2e0823deced1fff6ef8853
       },
       body: JSON.stringify(updatedDonor),
     });
 
     if (response.ok) {
-<<<<<<< HEAD
-      console.log("Donor updated successfully!");
-    } else {
-      console.error("Failed to update donor:", await response.text());
-      throw new Error(await response.text());
-    }
-  } catch (error) {
-    console.error("Error updating donor:", error);
-=======
       console.log('Donor updated successfully!');
     } else {
       console.error('Failed to update donor:', await response.text());
@@ -125,7 +74,6 @@ export const updateDonor = async (updatedDonor: Donor): Promise<void> => {
     }
   } catch (error) {
     console.error('Error updating donor:', error);
->>>>>>> 02e2af05956d7407bf2e0823deced1fff6ef8853
     throw error;
   }
 };
