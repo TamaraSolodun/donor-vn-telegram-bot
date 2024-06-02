ba988ade4168e29a7a676370766d31bb3fa9ca8b
Tamara Solodun <toma.solodun@gmail.com>
2024-06-02 13:50:39 +0300

    mongo secrets moved to .env

--- bot/config.js
+++ bot/config.js
@@ -1,5 +1,6 @@
+require('dotenv').config({ path: '../.env' });
+
 module.exports = {
-  token: '6923650844:AAER4Eb1Qm7H5iXMPe7BtkAk6HbLwOKdwOE',
-  mongoURI:
-    'mongodb+srv://donation:5H3Cusbio3G1D63X@cluster0.drv9hc8.mongodb.net/DonorsVN',
+  token: process.env.TOKEN,
+  mongoURI: process.env.MONGO_URI,
 };
